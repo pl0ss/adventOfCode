805d47ee76f0999d87c3de008fb1f7783565bb00
Kevin Ploß <65730791+pl0ss@users.noreply.github.com>
2024-12-03 18:04:47 +0100

    2024 Day 1

---
+++ 2024/day1/1/script.js
@@ -0,0 +1,54 @@
+const fs = require('fs/promises');
+
+const file_path = '2024/day1/data.txt';
+
+async function init() {
+    const data = await read_file(file_path);
+    const result = analyse_data(data);
+    console.log(result);
+}
+
+init();
+
+async function read_file(file_path) {
+    try {
+        const data = await fs.readFile(file_path, 'utf8');
+        return data;
+    } catch (err) {
+        console.error('Error reading file:', err);
+        throw err;
+    }
+}
+
+function analyse_data(data) {
+    const left = [];
+    const right = [];
+
+    const lines = data.split('\n');
+
+    for (let index = 0; index < lines.length; index++) {
+        const line = lines[index];
+        const parts = line.split(' ');
+        left.push(Number(parts.at(0)));
+        right.push(Number(parts.at(-1)));
+    }
+
+    left.sort((a, b) => a - b);
+    right.sort((a, b) => a - b);
+
+    if(left.length != right.length) {
+        console.error('left.length != right.length');
+    }
+
+    let sum = 0;
+
+    for (let index = 0; index < left.length; index++) {
+        const l = left[index];
+        const r = right[index];
+        
+        const x = Math.abs(l - r);
+        sum += x;
+    }
+    
+    return sum;
+}
